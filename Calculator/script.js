5a35c8b0302bc770d056e922c269f4dcc8ea572c
Lungastafa2 <lungastafa@gmail.com>
2026-02-02 22:36:05 +0200

    now we've added js and logic is working

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,1 +1,46 @@
-console.log("JS connected");
+const display = document.getElementById("screen");
+const buttons = document.querySelectorAll(".btn");
+
+let currentInput = "";
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const value = button.textContent;
+
+    // Clear
+    if (button.classList.contains("clear")) {
+      currentInput = "";
+      display.value = 0;
+      return;
+    }
+
+    // Equals
+    if (button.classList.contains("equals")) {
+      try {
+        currentInput = eval(currentInput);
+        display.value = currentInput;
+      } catch {
+        display.value = "Error";
+        currentInput = "";
+      }
+      return;
+    }
+
+
+    //delete 
+    if (button.classList.contains("delete")) {
+        
+      
+        currentInput = currentInput.slice(0, -7);
+        display.value = currentInput;
+        return;
+      }
+
+
+    // Normal input
+    currentInput += value;
+    display.value = currentInput;
+
+    
+  });
+});
